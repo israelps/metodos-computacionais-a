7c090358058462dc956cd0e47121d594c464af8a
leonardo <leonardo@cluster.(none)>
2017-06-23 16:56:45 -0300

    alteracao na funcao gamma

--- tarefa3 - Gamma/gamma.py
+++ tarefa3 - Gamma/gamma.py
@@ -1,16 +1,16 @@
 '''Escrito em python3'''
 from math import sqrt,pi,pow,exp
 
-# cabeçalho
+# cabecalho
 print('-----------------------------------------------------------')
 print('-------------  Aluno: Israel P. Siqueira  -----------------')
 print('-----------------Matricula: 14/0144587  -------------------')
 print('-----------------------------------------------------------')
-print('--------------  Tarefa 03 - Função Gamma ------------------')
+print('--------------  Tarefa 03 - Funcao Gamma ------------------')
 print('-----------------------------------------------------------')
 print('')
 
-#função que recebe (x+1) e calcula a função gama naquele ponto
+#funcao que recebe (x+1) e calcula a funcao gama naquele ponto
 def gama(x):
     return sqrt(2*pi*x)*pow(x,x)*exp(-x)*(
         1+1/(12*x)+1/(288*x**2)-139/(51840*x**3))
@@ -18,7 +18,7 @@ def gama(x):
 #arquivo de entrada
 f = open('input.txt')
 
-#loop que percorre o arquivo de entrada e calcula a função no ponto
+#loop que percorre o arquivo de entrada e calcula a funcao no ponto
 for linha in f:
     x = float(linha)
     print('Gamma(%.2f) = %.4f'%(x,gama(x)))
